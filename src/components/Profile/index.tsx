3eb836c6315ccb17abd660bc86360242a5e5e9c1
masonschafercodes <indytechbox@gmail.com>
2022-08-23 00:21:34 -0400

    chore: add more integrations manually

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -13,14 +13,50 @@ const integrations: IIntegrationData[] = [
     {
         integrationName: 'Slack',
         integrationDescription: 'Boost your productivity with your coworkers via chat and video messaging',
-        integrationImage: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.freebiesupply.com%2Flogos%2Flarge%2F2x%2Fslack-logo-icon.png&f=1&nofb=1',
+        integrationImage: 'https://asset.brandfetch.io/idJ_HhtG0Z/idS8F1wmDj.jpeg?updated=1646246424535',
         isConnected: false
     },
     {
         integrationName: 'clearbit',
         integrationDescription: 'Boost your productivity with your coworkers via chat and video messaging',
-        integrationImage: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fclipground.com%2Fimages%2Fclearbit-logo-7.jpg&f=1&nofb=1',
-        isConnected: true
+        integrationImage: 'https://asset.brandfetch.io/idPfQccWRj/idZcHaRy4-.jpeg?updated=1635901775059',
+        isConnected: false
+    },
+    {
+        integrationName: 'Linear',
+        integrationImage: 'https://asset.brandfetch.io/iduDa181eM/idYYbqOlKi.png?updated=1635899407667',
+        integrationDescription: 'Linear allows users to manage software development and track bugs.',
+        isConnected: false
+    },
+    {
+        integrationName: 'Google Drive',
+        integrationImage: 'https://asset.brandfetch.io/id6O2oGzv-/idNEgS9h8q.jpeg?updated=1655734433950',
+        integrationDescription: 'Our mission is to organize the world’s information and make it universally accessible and useful.',
+        isConnected: false
+    },
+    {
+        integrationName: 'Asana',
+        integrationImage: 'https://asset.brandfetch.io/idxPi2Evsk/idxxtwI5Gl.jpeg?updated=1646239586536',
+        integrationDescription: 'Easily organize and plan workflows, projects, and more, so you can keep your team\'s work on schedule. Start using Asana as your work management tool today.',
+        isConnected: false
+    },
+    {
+        integrationName: 'Salesforce',
+        integrationImage: 'https://asset.brandfetch.io/idVE84WdIN/idSAK0pQtK.jpeg?updated=1646247085744',
+        integrationDescription: 'Salesforce is a global cloud computing company that develops CRM solutions and provides business software on a subscription basis.',
+        isConnected: false
+    },
+    {
+        integrationName: 'Salesloft',
+        integrationImage: 'https://asset.brandfetch.io/idLO9lFZj5/id8bueqGAT.jpeg?updated=1656930742716',
+        integrationDescription: 'Salesloft helps thousands of the world’s most successful selling teams drive more revenue with the Modern Revenue Workspace.',
+        isConnected: false
+    },
+    {
+        integrationName: 'Salesvue',
+        integrationImage: 'https://asset.brandfetch.io/id_sVXWl5Q/id7HLnwdi0.png?updated=1635904561974',
+        integrationDescription: 'Prospecting to Pipeline Enablement',
+        isConnected: false
     }
 ]
 
