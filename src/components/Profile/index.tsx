08d6a289f4c07582cc53fa7fd200294d27037e3a
masonschafercodes <indytechbox@gmail.com>
2022-08-24 23:32:09 -0400

    prettier reformating

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,78 +1,77 @@
 import React from 'react';
-import {signOut} from "next-auth/react";
+import { signOut } from "next-auth/react";
 import IntegrationConnect from "~/components/Profile/IntegrationConnect";
-import {Integration} from '@prisma/client';
+import { Integration } from '@prisma/client';
 
 interface IIntegrationData {
-    integrationName: string;
-    integrationDescription: string;
-    integrationImage: string;
-    isConnected: boolean;
+  integrationName: string;
+  integrationDescription: string;
+  integrationImage: string;
+  isConnected: boolean;
 }
 
 const integrations: IIntegrationData[] = [
-    {
-        integrationName: 'clearbit',
-        integrationDescription: 'Clearbit develops business intelligence to help companies find more information of customers in order to increase sales and reduce fraud.',
-        integrationImage: 'https://asset.brandfetch.io/idPfQccWRj/idZcHaRy4-.jpeg?updated=1635901775059',
-        isConnected: false
-    },
-    {
-        integrationName: 'Salesforce',
-        integrationImage: 'https://asset.brandfetch.io/idVE84WdIN/idSAK0pQtK.jpeg?updated=1646247085744',
-        integrationDescription: 'Salesforce is a global cloud computing company that develops CRM solutions and provides business software on a subscription basis.',
-        isConnected: false
-    }
-]
+  {
+    integrationName: 'clearbit',
+    integrationDescription: 'Clearbit develops business intelligence to help companies find more information of customers in order to increase sales and reduce fraud.',
+    integrationImage: 'https://asset.brandfetch.io/idPfQccWRj/idZcHaRy4-.jpeg?updated=1635901775059',
+    isConnected: false
+  },
+  {
+    integrationName: 'Salesforce',
+    integrationImage: 'https://asset.brandfetch.io/idVE84WdIN/idSAK0pQtK.jpeg?updated=1646247085744',
+    integrationDescription: 'Salesforce is a global cloud computing company that develops CRM solutions and provides business software on a subscription basis.',
+    isConnected: false
+  }]
 
 interface Props {
-    userIntegrations: Integration[];
+  userIntegrations: Integration[];
 }
 
 export function Profile(props: Props) {
 
-    const {userIntegrations} = props;
+  const { userIntegrations } = props;
 
-    function isIntegrationConnected(userIntegrations: Integration[], standardIntegrationName: string) {
-        return userIntegrations?.some(integration => integration.type === standardIntegrationName);
-    }
+  function isIntegrationConnected(userIntegrations: Integration[], standardIntegrationName: string) {
+    return userIntegrations?.some(integration => integration.type === standardIntegrationName);
+  }
 
-    return (
-        <div className='max-w-6xl mx-auto'>
-            <div>
-                <div className="navbar justify-between bg-base-100">
-                    <a className="btn btn-ghost normal-case text-xl">DataTrak</a>
-                    <div className='flex gap-2'>
-                        <a href='/profile' className="btn btn-ghost btn-sm normal-case">
-                            Profile
-                        </a>
-                        {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-                        <a href='/dashboard' className="btn btn-ghost btn-sm normal-case">
-                            Dashboard
-                        </a>
-                        <button
-                            onClick={() => signOut({callbackUrl: "/"})}
-                            className="btn btn-ghost btn-sm normal-case"
-                        >
-                            Logout
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div className='mt-2'>
-                <div className='my-2'>
-                    <h1 className='text-xl font-semibold text-gray-300'>Integrations</h1>
-                </div>
-                <div className='grid grid-cols-2 gap-2'>
-                    {integrations.map((integration, index) => (
-                        <IntegrationConnect integrationName={integration.integrationName}
-                                            integrationDescription={integration.integrationDescription}
-                                            integrationImage={integration.integrationImage}
-                                            isConnected={isIntegrationConnected(userIntegrations, integration.integrationName)}
-                                            key={index}/>
-                    ))}
-                </div>
-            </div>
+  return (
+    <div className='max-w-6xl mx-auto'>
+      <div>
+        <div className="navbar justify-between bg-base-100">
+          <a className="btn btn-ghost normal-case text-xl">DataTrak</a>
+          <div className='flex gap-2'>
+            <a href='/profile' className="btn btn-ghost btn-sm normal-case">
+              Profile
+            </a>
+            {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+            <a href='/dashboard' className="btn btn-ghost btn-sm normal-case">
+              Dashboard
+            </a>
+            <button
+              onClick={() => signOut({ callbackUrl: "/" })}
+              className="btn btn-ghost btn-sm normal-case"
+            >
+              Logout
+            </button>
+          </div>
         </div>
-    )
+      </div>
+      <div className='mt-2'>
+        <div className='my-2'>
+          <h1 className='text-xl font-semibold text-gray-300'>Integrations</h1>
+        </div>
+        <div className='grid grid-cols-2 gap-2'>
+          {integrations.map((integration, index) => (
+            <IntegrationConnect integrationName={integration.integrationName}
+              integrationDescription={integration.integrationDescription}
+              integrationImage={integration.integrationImage}
+              isConnected={isIntegrationConnected(userIntegrations, integration.integrationName)}
+              key={index} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/components/UI/SavedSearch.tsx
+++ src/components/UI/SavedSearch.tsx
@@ -1,33 +1,33 @@
 import React from 'react';
-import {ISavedSearch} from "~/components/Dashboard";
+import { ISavedSearch } from "~/components/Dashboard";
 import Modal from "~/components/UI/Modal";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 interface Props {
-    savedSearch: ISavedSearch;
+  savedSearch: ISavedSearch;
 }
 
 function SavedSearch(props: Props) {
-    return (
-        <div className='p-3 border border-gray-600 rounded-lg shadow-lg'>
-            <div>
-                <h3 className='text-gray-500 font-bold'>Query: <span className='font-normal text-gray-300'>{props.savedSearch.query}</span></h3>
-                <p className='text-gray-500 text-sm italic'>Searched
-                    at: {new Date(props.savedSearch.createdAt).toLocaleDateString()}</p>
-            </div>
-            <div className='mt-3'>
-                <Modal modalButtonText='View Result Output'
-                       modalName={`${props.savedSearch.query}-query-output-modal-${new Date(props.savedSearch.createdAt).toLocaleDateString()}`} modalSize='lg'>
-                    <div>
-                        <SyntaxHighlighter language='json' style={dracula}>
-                            {JSON.stringify(props.savedSearch.result, null, 2)}
-                        </SyntaxHighlighter>
-                    </div>
-                </Modal>
-            </div>
-        </div>
-    );
+  return (
+    <div className='p-3 border border-gray-600 rounded-lg shadow-lg'>
+      <div>
+        <h3 className='text-gray-500 font-bold'>Query: <span className='font-normal text-gray-300'>{props.savedSearch.query}</span></h3>
+        <p className='text-gray-500 text-sm italic'>Searched
+          at: {new Date(props.savedSearch.createdAt).toLocaleString()}</p>
+      </div>
+      <div className='mt-3'>
+        <Modal modalButtonText='View Result Output'
+          modalName={`${props.savedSearch.query}-query-output-modal-${new Date(props.savedSearch.createdAt).toLocaleTimeString()}`} modalSize='lg'>
+          <div>
+            <SyntaxHighlighter language='json' style={dracula}>
+              {JSON.stringify(props.savedSearch.result, null, 2)}
+            </SyntaxHighlighter>
+          </div>
+        </Modal>
+      </div>
+    </div>
+  );
 }
 
 export default SavedSearch;
